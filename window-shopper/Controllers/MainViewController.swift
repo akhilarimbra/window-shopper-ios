f3a86d56f58586ac49412640460448cf07faffb1
Akhil Raj Arimbra <nilamburakhil@gmail.com>
2018-07-25 04:01:28 +0530

    added wage calculation

--- window-shopper/Controllers/MainViewController.swift
+++ window-shopper/Controllers/MainViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class MainViewController: UIViewController {
     @IBOutlet weak var wageTextField: CurrencyTextField!
     @IBOutlet weak var priceTextField: CurrencyTextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var hoursLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,11 +25,29 @@ class MainViewController: UIViewController {
         
         wageTextField.inputAccessoryView = calculateButton
         priceTextField.inputAccessoryView = calculateButton
+        
+        resultLabel.isHidden = true
+        hoursLabel.isHidden = true
     }
     
     @objc func calculate() {
-        print("We got here")
-        
+        if let wageText = wageTextField.text, let priceText = priceTextField.text {
+            if let wage = Double(wageText), let price = Double(priceText) {
+                view.endEditing(true)
+                
+                resultLabel.isHidden = false
+                hoursLabel.isHidden = false
+                
+                resultLabel.text = "\(Wage.getHours(forWage: wage, andPrice: price))"
+            }
+        }
+    }
+    
+    @IBAction func clearCalculator(_ sender: Any) {
+        resultLabel.isHidden = true
+        hoursLabel.isHidden = true
+        wageTextField.text = ""
+        priceTextField.text = ""
     }
     
 }
---
+++ window-shopper/Models/Wage.swift
@@ -0,0 +1,15 @@
+//
+//  Wage.swift
+//  window-shopper
+//
+//  Created by Akhil Raj Arimbra on 7/25/18.
+//  Copyright © 2018 Akhil Raj Arimbra. All rights reserved.
+//
+
+import Foundation
+
+class Wage {
+    class func getHours(forWage wage: Double, andPrice price: Double) -> Int {
+        return Int(ceil(price / wage))
+    }
+}
