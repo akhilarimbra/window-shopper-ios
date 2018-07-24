17778a658d89d3da0b1e3dccd540b0f3af2abbb3
Akhil Raj Arimbra <nilamburakhil@gmail.com>
2018-07-25 03:01:13 +0530

    added calculateButton()

--- window-shopper/Controllers/MainViewController.swift
+++ window-shopper/Controllers/MainViewController.swift
@@ -9,9 +9,25 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    @IBOutlet weak var wageTextField: CurrencyTextField!
+    @IBOutlet weak var priceTextField: CurrencyTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let calculateButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calculateButton.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        calculateButton.setTitle("Calculate", for: .normal)
+        calculateButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calculateButton.addTarget(self, action: #selector(MainViewController.calculate), for: .touchUpInside)
+        
+        wageTextField.inputAccessoryView = calculateButton
+        priceTextField.inputAccessoryView = calculateButton
+    }
+    
+    @objc func calculate() {
+        print("We got here")
+        
     }
     
 }
