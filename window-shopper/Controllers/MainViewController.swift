8c44ebe000df5acba8b70ef6495afa4657f45bb0
Akhil Raj Arimbra <nilamburakhil@gmail.com>
2018-07-25 02:26:53 +0530

    added assets

---
+++ window-shopper/Controllers/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Akhil Raj Arimbra on 7/25/18.
+//  Copyright © 2018 Akhil Raj Arimbra. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
---
+++ window-shopper/Views/CurrencyTextField.swift
@@ -0,0 +1,20 @@
+//
+//  CurrencyTextField.swift
+//  window-shopper
+//
+//  Created by Akhil Raj Arimbra on 7/25/18.
+//  Copyright © 2018 Akhil Raj Arimbra. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.25)
+        
+    }
+    
+}
